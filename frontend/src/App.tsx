cf54c51e5436b03a94e21647a9ef7ca2740cbe72
lim <dntksql1010@naver.com>
2025-07-02 23:27:00 +0900

    feat: 라우터 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import PostList from './pages/PostList';
+import PostForm from './pages/PostForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to='/'>글 목록</Link> | <Link to='/write'>글 작성</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<PostList />} />
+        <Route path='/write' element={<PostForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
